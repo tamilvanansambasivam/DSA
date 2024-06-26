eb5e7d3e2d252e294764c096bf2d054ecbc07697
tamilvanan <tamilvanansambasivam@gmail.com>
2024-06-26 15:51:11 +0530

    add array data structure

---
+++ Arrays/index.js
@@ -0,0 +1,4 @@
+// Example of creating and accessing elements in an array
+let arr = [1, 2, 3, 4, 5];
+console.log(arr[0]); // Accessing the first element
+console.log(arr[2]); // Accessing the third element
