2b9ac06244b9d6b043f7664c8e4effc2c500b8fd
tamilvanan <tamilvanansambasivam@gmail.com>
2024-11-19 16:13:47 +0530

    simple array

---
+++ array/index.js
@@ -0,0 +1,9 @@
+// an array is a collection of elements stored in continuous memory location.
+
+const arr = [1, 2, 3, 4, 5];
+
+const mixed = [1, true, "hello", 2, undefined, 9];
+
+console.log(arr);
+
+console.log(mixed);
